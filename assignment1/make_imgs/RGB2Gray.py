c240272ec9d11e67e815e9ea2b984bedef78d6f4
lizhounan <lizhounan0411@gmail.com>
2021-01-29 20:11:21 -0500

    assignment1 finished

--- assignment1/make_imgs/RGB2Gray.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image
-import glob
-filenames = glob.glob('*.jpg')
-print(filenames)
-# img = Image.open('image.png').convert('LA')
-for file in filenames:
-	img = Image.open(file).convert('LA')
-	img.save('gray'+file[0] + '.png')
